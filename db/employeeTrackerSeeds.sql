05152596e6c11f53c78b59a759963a9f5be8103f
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-05-23 23:09:26 -0400

    Starting

--- db/employeeTrackerSeeds.sql
+++ db/employeeTrackerSeeds.sql
@@ -7,4 +7,4 @@ values ("Production Manager", 65000, 1), ("Manufactoring Production Planner", 59
 ("Purchasing Agent", 65000, 3),("Director of Purchasing",150000,3), ("Purchasing Manager", 85000,3), ("Purchsing Assistant",42000,3),
 ("Human Resource Manager", 75000,5), ("Human Resource Generalist", 47000,5), ("Director of Human Resource", 120000, 5),
 ("Director of Finance and Accounting", 140000, 6), ("Accounting Specialist", 42000, 6), ("Accounting Manager", 68000, 6),
-("Chief Executive Officer", 150000, 7), ("Chief Financial Officer", 150000,7), ("Chief Operations Officer", 150000, 7)
+("Chief Executive Officer", 150000, 7), ("Chief Financial Officer", 150000,7), ("Chief Operations Officer", 150000, 7);
